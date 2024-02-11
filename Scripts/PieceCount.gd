9444ee12b3491a0cb271328dd6b23f153eb59793
Jayden Liddelow <jaydlidd132@gmail.com>
2024-02-11 13:33:30 +1100

    11/2 :wrench: Reformatted script and setup multiplayer testing scene
    + Moved comments in GameManager
    + Added new scene to begin multi player testing

--- Scripts/PieceCount.gd
+++ Scripts/PieceCount.gd
@@ -4,7 +4,7 @@ var piece_count: int
 var total_count: int
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	piece_count = GlobalVars.get_piece_count(1)
 	total_count = GlobalVars.get_total_pieces(1)
 	
